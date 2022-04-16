6bf455c74d9b0074c741180a5f9d10abdf2aefc6
刘启明 <455043818@qq.com>
2022-04-16 20:22:17 +0800

    Update App.vue

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -37,32 +37,73 @@ const demo1 = () => {
   sub.emit('放假', '顶顶顶')
 }
 
-const demo2 = () => {
-  // 定义一个目标对象
-  const subject = new Subject()
-  // 观察者
-  const stu1 = new Observer('弟子-无奈')
+  const demo2 = () => {
+    // 定义一个目标对象
+    const subject = new Subject()
+    // 观察者
+    const stu1 = new Observer('弟子-无奈')
 
-  // 观察者
-  const stu2 = new Observer('弟子-无心')
+    // 观察者
+    const stu2 = new Observer('弟子-无心')
 
-  subject.addObserver(stu1)
-  subject.addObserver(stu2)
+    subject.addObserver(stu1)
+    subject.addObserver(stu2)
 
-  const warTask = {
-    taskType :'war',
-    taskInfo : '猎杀时刻'
+    const warTask = {
+      taskType :'war',
+      taskInfo : '猎杀时刻'
+    }
+
+    subject.notify(warTask)
   }
 
-  subject.notify(warTask)
+// new Function
+const demo3 = () => {
+
+  let c = 30
+  const getTemp = () => {
+    return 60
+  }
+  let code = `return a + b+ ${c} + ${getTemp()}`
+
+  let fun = new Function('a', 'b', code)
+  console.log(fun(10, 20))
+}
+
+// eval使用
+const demo4 = () => {
+  let c = 30
+  const getTemp = () => {
+    return 60
+  }
+  let code = `(a,b) =>{
+    return a + b+ ${c} + ${getTemp()} 
+  }`
+
+  const fun = eval(code)
+  console.log(typeof fun)
+  console.log(fun)
+  console.log(fun(10,20))
+}
+
+
+const demo44 = () => {
+  eval(`console.log('hello world')`)   // hello world
+
+  let value = 100
+  eval(`console.log(${value}+100)`)   // 200
+
+
 }
 
 // demo1()
-demo2()
+// demo2()
+// demo3()
+demo4()
+// demo44()
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
 </template>
 
 <style>
